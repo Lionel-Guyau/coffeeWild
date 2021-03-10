64d21037eeb3f80d0c06480b710c436557e72a76
David Bourrel <david-bourrel@hotmail.fr>
2021-03-10 13:35:02 +0100

    Ajout de la fonction toggle

--- js/app.js
+++ js/app.js
@@ -1,6 +1,44 @@
-const App = function() {
-  'use strict'
+const App = function () {
+  "use strict";
 
   /* Click Dark Mode */
-}
+  let label = document.querySelector(".switch");
+  let slider = document.querySelector(".slider");
+  
+  label.addEventListener("click", function() {
+    document.body.classList.toggle("darkMode");
+    slider.classList.toggle("darkMode");
+  })
+
+  /* Test Coffee Wild */
+  const inputCoffee = document.getElementById("inputCoffee");
+  const inputAge = document.getElementById("inputAge");
+  const submitButton = document.getElementById("submit");
+  const formCoffee = document.getElementById("formCoffee");
+  
+
+  submitButton.disabled = true;
+  inputCoffee.onkeyup = () => {
+    if (inputCoffee.value.length && inputAge.value.length > 0) {
+      submitButton.disabled = false;
+    } else {
+      submitButton.disabled = true;
+    }
+  };
+/*
+  formCoffee.onsubmit = function(e) {
+    e.preventDefault();
+
+    inputCoffee.disabled = true
+    submitButton.disabled = true
+
+    let CaféDepuisLaNaissance = document.getElementById("");
+    let CaféRestantJusquaLaMort = document.getElementById("");
+    let totalCaféDansUneVie = document.getElementById("");
+    let tauxDeCaféine = document.getElementById("");
+    let convertionCaféAlHeure = document.getElementById("");
+    
+    
+  }*/
+};
 App();
